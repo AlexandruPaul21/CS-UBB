7bd0ab468183a31594cf39e2cc47fc73e118dd99
Alexandru Sirbu <Alexandru.Sirbu@cloudflight.io>
2023-11-06 23:40:56 +0200

    LFTC: Lab02

---
+++ Year03/1st semester/LFTC/Lab/Lab02/Lab02-Code/src/main/java/org/example/FiniteStateMachine.java
@@ -0,0 +1,114 @@
+package org.example;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FiniteStateMachine {
+    private Set<String> states;
+    private Set<String> alphabet;
+    private List<Transition> transitions;
+    private String initialState;
+    private Set<String> finalStates;
+
+    private Set<String> letters = new HashSet<>();
+    private Set<String> digits = new HashSet<>();
+
+    public FiniteStateMachine(Set<String> states, Set<String> alphabet, List<Transition> transitions, String initialState, Set<String> finalStates) {
+        this.states = states;
+        this.alphabet = alphabet;
+        this.transitions = transitions;
+        this.initialState = initialState;
+        this.finalStates = finalStates;
+
+        Collections.addAll(letters, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+        Collections.addAll(digits, "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+    }
+
+    private String matchExpression(String sequence) {
+        StringBuilder result = new StringBuilder();
+        String actualState = initialState;
+
+        for (int index = 0; index < sequence.length(); ++index) {
+            String finalActualState = actualState;
+            List<Transition> validTransitions = transitions.stream().filter(transition -> transition.getInitialState().equals(finalActualState)).toList();
+
+            int finalIndex = index;
+            List<Transition> validTransition = validTransitions.stream().filter(transition -> Objects.equals(transition.getValue(), String.valueOf(sequence.charAt(finalIndex)))).toList();
+
+            if (validTransition.size() != 1) {
+                return result.toString();
+            }
+            result.append(sequence.charAt(index));
+
+            actualState = validTransition.get(0).getFinalState();
+        }
+
+        String finalActualState1 = actualState;
+        if (finalStates.stream().anyMatch(state -> state.equals(finalActualState1))) {
+            return result.toString();
+        } else {
+            return null;
+        }
+    }
+
+    public boolean checkCompliance(String sequence) {
+        return Objects.equals(matchExpression(sequence), sequence);
+    }
+
+    public String checkOffset(String sequence) {
+        return matchExpression(sequence);
+    }
+
+    public Set<String> getStates() {
+        return states;
+    }
+
+    public void setStates(Set<String> states) {
+        this.states = states;
+    }
+
+    public Set<String> getAlphabet() {
+        return alphabet;
+    }
+
+    public void setAlphabet(Set<String> alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    public List<Transition> getTransitions() {
+        return transitions;
+    }
+
+    public void setTransitions(List<Transition> transitions) {
+        this.transitions = transitions;
+    }
+
+    public String getInitialState() {
+        return initialState;
+    }
+
+    public void setInitialState(String initialState) {
+        this.initialState = initialState;
+    }
+
+    public Set<String> getFinalStates() {
+        return finalStates;
+    }
+
+    public void setFinalStates(Set<String> finalStates) {
+        this.finalStates = finalStates;
+    }
+
+    @Override
+    public String toString() {
+        return "FiniteStateMachine{" +
+                "states=" + states +
+                ", alphabet=" + alphabet +
+                ", transitions=" + transitions +
+                ", initialState='" + initialState + '\'' +
+                ", finalStates=" + finalStates +
+                ", letters=" + letters +
+                ", digits=" + digits +
+                '}';
+    }
+}
---
+++ Year03/1st semester/LFTC/Lab/Lab02/Lab02-Code/src/main/java/org/example/Main.java
@@ -0,0 +1,149 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+/*
+* Structura in EBNF
+*
+* automat_finit = lista_stari, stare_initiala, stari_finale, tranzitii.
+* lista_stari & stari_finale = stare {" " , stare}.
+* stare_initiala = stare
+* tranzitii = tranzitie { "\n", tranzitie }
+* stare = q,CONST
+* tranzitie = stare CONST stare
+* CONST = digit{digit}
+* digit = [0-9]
+*
+* */
+public class Main {
+
+    private static FiniteStateMachine af;
+
+    private static void readFile(String file) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            Set<String> states = Arrays.stream(bufferedReader.readLine().split(" ")).collect(Collectors.toSet());
+            String initialState = bufferedReader.readLine();
+            Set<String> finalStates = Arrays.stream(bufferedReader.readLine().split(" ")).collect(Collectors.toSet());
+            Set<String> alphabet = Arrays.stream(bufferedReader.readLine().split(" ")).collect(Collectors.toSet());
+
+            List<Transition> transitions = new ArrayList<>();
+            String transition = bufferedReader.readLine();
+            while (transition != null) {
+                String[] elements = transition.split(" ");
+                transitions.add(new Transition(elements[0], elements[2], elements[1]));
+
+                transition = bufferedReader.readLine();
+            }
+
+            af = new FiniteStateMachine(states, alphabet, transitions, initialState, finalStates);
+
+            System.out.println("Finite state machine successfully read from file");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void readConsole() {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> states = new HashSet<>();
+        System.out.println("Give the possible states: ");
+        Collections.addAll(states, scanner.nextLine().split(" "));
+
+        System.out.println("Give the initial state");
+        String initialState = scanner.nextLine();
+
+        Set<String> finalStates = new HashSet<>();
+        System.out.println("Give the final states: ");
+        Collections.addAll(finalStates, scanner.nextLine().split(" "));
+
+        Set<String> alphabet = new HashSet<>();
+        System.out.println("Give the alphabet: ");
+        Collections.addAll(alphabet, scanner.nextLine().split(" "));
+
+        System.out.println("Introduce the transition and than enter");
+        List<Transition> transitions = new ArrayList<>();
+        String transition = scanner.nextLine();
+        while (transition != null && !transition.isEmpty()) {
+            String[] elements = transition.split(" ");
+            transitions.add(new Transition(elements[0], elements[2], elements[1]));
+
+            transition = scanner.nextLine();
+        }
+
+        af = new FiniteStateMachine(states, alphabet, transitions, initialState, finalStates);
+
+        System.out.println("Finite state machine successfully read from console");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--------------------Welcome-------------------");
+        System.out.println("How do you want to input the state machine?");
+        System.out.println("\t1. File");
+        System.out.println("\t2. Console");
+        System.out.println("\t3. Exit");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+        switch (command) {
+            case "1" -> readFile("input.txt");
+            case "2" -> readConsole();
+            case "3" -> {
+                System.out.println("Goodbye!");
+                return;
+            }
+            default -> {
+                System.out.println("Error! Exiting......");
+                return;
+            }
+        }
+
+        boolean done = false;
+
+        while (!done) {
+            System.out.println("----------Functionalities------------------");
+            System.out.println("\t1. States");
+            System.out.println("\t2. Input alphabet");
+            System.out.println("\t3. Transitions");
+            System.out.println("\t4. Final states");
+            System.out.println("\t5. Check the compliance of a sequence");
+            System.out.println("\t6. Determine the longest compliant offset");
+            System.out.println("\t0. Exit");
+
+            command = scanner.nextLine();
+            switch (command) {
+                case "1" -> System.out.println(af.getStates());
+                case "2" -> System.out.println(af.getAlphabet());
+                case "3" -> af.getTransitions().forEach(System.out::println);
+                case "4" -> af.getFinalStates().forEach(System.out::println);
+                case "5" -> {
+                    System.out.println("Introduce the sequence: ");
+                    String sequence = scanner.nextLine();
+                    if (af.checkCompliance(sequence)) {
+                        System.out.println("The sequence is compliant");
+                    } else {
+                        System.out.println("The sequence is not compliant");
+                    }
+                }
+                case "6" -> {
+                    System.out.println("Introduce the sequence: ");
+                    String sequence = scanner.nextLine();
+                    String result = af.checkOffset(sequence);
+                    System.out.println(result == null ? sequence : result);
+                }
+                case "0" -> {
+                    System.out.println("Goodbye!");
+                    done = true;
+                }
+                default -> {
+                    System.out.println("Error! Exiting......");
+                    return;
+                }
+            }
+        }
+    }
+}
